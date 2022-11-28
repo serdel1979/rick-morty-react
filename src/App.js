2401f7f689b72e839799fa6d53ab02e6e7705345
unknown <sdlbsso@gmail.com>
2022-11-28 11:12:05 -0300

    app básica react con api pública rick y morty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import imageRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+
+  const [characters, setCharacters ] = useState([]);
+
+
+  
+
+  const reqApi = async ()=>{
+    const resp = await fetch('https://rickandmortyapi.com/api/character')
+    const { results } = await resp.json()
+    setCharacters((results)=>results);
+    console.log('--',characters);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title'>Rick & Morty</h1>
+        <img src={imageRickMorty} alt='Rick & Morty' className='img-home'/>
+      
+        <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
       </header>
     </div>
   );
